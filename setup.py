4cde688e541dabede95040ab540e38a4cceedc26
Alessio <randomcoil@protonmail.com>
2025-12-16 15:28:21 +0100

    Improved compatibility with latest Python and Numpy versions

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 import setuptools
 
-with open("README.md", "r",encoding="utf8") as fh:
+with open("README.md", "r", encoding="utf8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="Graph Sampling",
     version="0.0.1",
-    authors=["Kirti Jain","Ashish Aggarwal"],
-    authors_email=["kirtij.mcs16.du@gmail.com","ashish.mcs16.du@gmail.com"],
+    authors=["Kirti Jain", "Ashish Aggarwal"],
+    authors_email=["kirtij.mcs16.du@gmail.com", "ashish.mcs16.du@gmail.com"],
     description="Graph Sampling Package",
     long_description="A Graph Sampling package containing various approaches which samples the original graph according to different sample sizes",
     long_description_content_type="text/markdown",
